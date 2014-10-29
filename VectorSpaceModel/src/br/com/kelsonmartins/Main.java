101ce33810289cf9ba89d5d3a54e061da0263a1f
Kelson Martins <kelson.martins@outlook.com>
2014-10-29 20:21:35 +0000

    Raw Parser Phase Finished. Next Step: Adapting and organize it in Arrays

--- VectorSpaceModel/src/br/com/kelsonmartins/Main.java
+++ VectorSpaceModel/src/br/com/kelsonmartins/Main.java
@@ -21,10 +21,9 @@ package br.com.kelsonmartins;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.Character.Subset;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
 
@@ -34,7 +33,9 @@ public class Main {
 		  
 		  BufferedReader br = new BufferedReader(new FileReader("corpus.txt"));
 		  
-		  List<String[]> data = new ArrayList<String[]>();
+		  List<String> data = new ArrayList<String>(); 
+		  String[] temp;
+
 		  
 		  int i,t,a,b,w = 0;
 		  
@@ -56,46 +57,56 @@ public class Main {
 		        System.out.println(everything.indexOf(".I"));
 		        
 		        i = everything.indexOf(".I");
+		        int it = 0;
 		        
-		        //while (i != -1) {
+		        while (i != -1) {
 		        	
-		        //}
-		        
-		        System.out.println(everything.indexOf(".T"));
-		        
-		        t = everything.indexOf(".T");
-		        
-		        System.out.println(everything.indexOf(".A"));
-		        
-		        a = everything.indexOf(".A");
-		        
-		        System.out.println(everything.indexOf(".B"));
-		        
-		        b = everything.indexOf(".B");
-		        
-		        System.out.println(everything.indexOf(".W"));
-		        
-		        w = everything.indexOf(".W");
-		        
-		        System.out.println( everything.substring(i + separator, t) );
-		        System.out.println( everything.substring(t + separator, a) );
-		        System.out.println( everything.substring(a + separator, b) );
-		        System.out.println( everything.substring(b + separator, w) );
+		        	it++;
+			        System.out.println(everything.indexOf(".T"));
+			        
+			        t = everything.indexOf(".T");
+			        
+			        System.out.println(everything.indexOf(".A"));
+			        
+			        a = everything.indexOf(".A");
+			        
+			        System.out.println(everything.indexOf(".B"));
+			        
+			        b = everything.indexOf(".B");
+			        
+			        System.out.println(everything.indexOf(".W"));
+			        
+			        w = everything.indexOf(".W");
+			        
+			        System.out.println("Iteration " + it);
+			        
+			        System.out.println( everything.substring(i + separator, t) );
+			        System.out.println( everything.substring(t + separator, a) );
+			        System.out.println( everything.substring(a + separator, b) );
+			        System.out.println( everything.substring(b + separator, w) );
+			        
+
+			        everything = everything.substring(w+separator);
+			        
+			        //System.out.println(everything);
+			        
+			        i = everything.indexOf(".I");
+
+			        if (i != -1) {
+			        	System.out.println( everything.substring(0, i) );
+				        //sI = everything.substring(i + separator, t);
+				        //sT = everything.substring(t + separator, a);
+				        //sA = everything.substring(a + separator, b);
+				        //sB = everything.substring(b + separator, w);
+				        //sW = everything.substring(w + separator , i);
+				        
+				        //temp = new String[]{sI,sT,sA,sB,sW};
+				        //Collections.addAll(data, temp);
+			        }
+		        	
+		        }
 		        
 
-		        everything = everything.substring(w+separator);
-		        
-		        //System.out.println(everything);
-		        
-		        i = everything.indexOf(".I");
-		        
-		        System.out.println( everything.substring(0, i) );
-		        
-		        //sI = everything.substring(i + separator, t);
-		        //sT = everything.substring(t + separator, a);
-		        //sA = everything.substring(a + separator, b);
-		        //sB = everything.substring(b + separator, w);
-		        //sW = everything.substring(w + separator , i);
 		        
 		        
 		    } finally {
